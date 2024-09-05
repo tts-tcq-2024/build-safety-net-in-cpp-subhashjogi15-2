0c8e7b7e4fdd1383e00ff1c6cc8d823d2411ef7f
subhashjogi15 <subhashjogi9184@gmail.com>
2024-09-05 13:18:05 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -2,8 +2,16 @@
 #include "Soundex.h"
 
 
-TEST(SoundexTest, isPrevLetterHWYPass) {
+TEST(SoundexTest, isPrevLetterHWY_Pass) {
     const std::string name = "TYMZAK";
     size_t i = 2;
     EXPECT_TRUE(isPrevLetterHWY(name, i));
 }
+
+TEST(SoundexTest, isPrevLetterHWY_Fail) {
+    const std::string name = "TYMZAK";
+    size_t i = 3;
+    EXPECT_FALSE(isPrevLetterHWY(name, i));
+}
+
+
