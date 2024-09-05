659e2aa91cd17b6bf830955eb62ce65d3afb092c
subhashjogi15 <subhashjogi9184@gmail.com>
2024-09-05 10:53:15 +0530

    Update Soundex.cpp
    1st changes

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,36 +1,80 @@
-#include "Soundex.h"
+#include <iostream>
+#include <string>
 #include <cctype>
+#include <map>
+using namespace std;
 
-char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
+bool isPrevLetterHWY(std::string& name, size_t& i) {
+  if ((name[i-1] == 'H' || name[i-1] == 'W' || name[i-1] == 'Y')) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
-std::string generateSoundex(const std::string& name) {
+char getSoundexCode(char c) {
+    std::map<char, char> soundexMap = {
+        {'A', '0'}, {'E', '0'}, {'I', '0'}, {'O', '0'}, {'U', '0'},
+        {'H', '0'}, {'W', '0'}, {'Y', '0'}, {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
+        {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'}, {'S', '2'}, {'X', '2'}, {'Z', '2'},
+        {'D', '3'}, {'T', '3'},
+        {'L', '4'},
+        {'M', '5'}, {'N', '5'},
+        {'R', '6'}
+    };
+    auto it = soundexMap.find(toupper(c));
+    return (it != soundexMap.end()) ? it->second : '0';
+}
+
+void generateSoundexForPrevLetterHWY(std::string& name, size_t& i, std::string& soundex, char& lastCode) {
+             char currCode = getSoundexCode(name[i]);
+           if(lastCode != currCode) {
+               soundex += currCode;
+  }
+}
+
+void generateSoundexForCurrentLetter(std::string& name, size_t& i, std::string& soundex, char& previousCode) {
+  if (getSoundexCode(name[i]) != '0' && getSoundexCode(name[i]) != previousCode) {
+    soundex += getSoundexCode(name[i]);
+  }
+}
+
+
+std::string generateSoundex(std::string& name) {
     if (name.empty()) return "";
-
+    
     std::string soundex(1, toupper(name[0]));
-    char prevCode = getSoundexCode(name[0]);
-
+    char previousLetter = toupper(name[0]);
+    char previousCode = getSoundexCode(name[0]);
+    
+    char lastLetter = toupper(name[0]);
+    char lastCode = getSoundexCode(name[0]);
+    
     for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
-            soundex += code;
-            prevCode = code;
+        if (isPrevLetterHWY(name, i)) {
+          generateSoundexForPrevLetterHWY(name, i, soundex, lastCode);
         }
+        else {
+          generateSoundexForCurrentLetter(name, i, soundex, previousCode);
+        }
+        lastCode = previousCode;
+        previousCode = getSoundexCode(name[i]);
     }
 
     while (soundex.length() < 4) {
         soundex += '0';
     }
+       return soundex;
+}
 
-    return soundex;
+int main() {
+std::string name;
+    std::cout << "Enter a name: ";
+    std::cin >> name;
+ 
+    std::string soundexCode = generateSoundex(name);
+    std::cout << "Soundex code for " << name << " is " << soundexCode << std::endl;
+ 
+    return 0;
 }
