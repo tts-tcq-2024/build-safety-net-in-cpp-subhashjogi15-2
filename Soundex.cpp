99bc2ed68fb51f04237bd20f96fc957ddd3cf4e3
subhashjogi15 <subhashjogi9184@gmail.com>
2024-09-23 12:18:12 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,11 +1,11 @@
 #include "Soundex.h"
 using namespace std;
  
-list<char> vowel = {'A','E','I','O','U'};
-list<char> letterHWY = {'H','W','Y'};
- 
-char getSoundexCode(char c) 
-{
+bool isPrevLetterHWY(const std::string& name, size_t& iterator) {
+  return (toupper(name[iterator-1]) == 'H' || toupper(name[iterator-1]) == 'W' || toupper(name[iterator-1]) == 'Y');
+}
+
+char getSoundexCode(char c) {
     std::map<char, char> soundexMap = {
         {'A', '0'}, {'E', '0'}, {'I', '0'}, {'O', '0'}, {'U', '0'},
         {'H', '0'}, {'W', '0'}, {'Y', '0'}, {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
@@ -18,64 +18,60 @@ char getSoundexCode(char c)
     auto it = soundexMap.find(toupper(c));
     return (it != soundexMap.end()) ? it->second : '0';
 }
- 
-bool checkIfLetterIsHWY(const std::string& name, size_t& index)
-{
-    char letterToFind = name[index - 1];
-    bool found = false;
-    for (char ch : letterHWY) {
-        if (ch == letterToFind) {
-            found = true;
-            break;
-        }
-    }
-    return found;
+
+void generateSoundexIfPrevLetterHWY(const std::string& name, size_t& iterator, std::string& soundex, char& lastCode) {
+  char currCode = getSoundexCode(name[iterator]);
+  if(lastCode != currCode) {
+     soundex += currCode;
+  }
 }
- 
-bool checkIfLetterIsVowel(const std::string& name, size_t& index)
-{
-    char letterToFind = name[index - 1];
-    bool found = false;
-    for (char ch : vowel) {
-        if (ch == letterToFind) {
-            found = true;
-            break;
-        }
-    }
-    return found;
+
+void generateSoundexIfPrevLetterNotHWY(const std::string& name, size_t& iterator, std::string& soundex, char& previousCode) {
+if (getSoundexCode(name[iterator]) != '0' && getSoundexCode(name[iterator]) != previousCode) {
+    char currCode = getSoundexCode(name[iterator]);
+    soundex += currCode;
+  }
 }
- 
-void checkForLetters(std::string& soundex, const std::string& name, size_t& index, char& code)
-{
-    if (!checkIfLetterIsHWY(name, index)) 
-    {
-        soundex += code;
-    }
-    else if (checkIfLetterIsVowel(name, index))
-    {
-        soundex += code;
+
+void makeSoundeLengthFour(std::string& soundex) {
+  while (soundex.length() < 4) {
+        soundex += '0';
     }
 }
- 
-void handelSoundex(std::string& soundex, const std::string& name, size_t& index, char& code, char& prevCode)
-{
-    if(code != prevCode && code != '0')
-    {
-     checkForLetters(soundex, name, index, code);
-    }
-    prevCode = code;
+
+void generateSoundexForLetter(const std::string& name, size_t& iterator, std::string& soundex, char& previousCode, char& lastCode) {
+  if (isPrevLetterHWY(name, iterator)) {
+    generateSoundexIfPrevLetterHWY(name, iterator, soundex, lastCode);
+  }
+  else {
+    generateSoundexIfPrevLetterNotHWY(name, iterator, soundex, previousCode);
+  }
 }
-void createSoundexCode(std::string& soundex, const std::string& name){
-    char prevCode = getSoundexCode(name[0]);
-    for (size_t index = 1; index <= name.length() && soundex.length() < 4; ++index) {
-        char code = getSoundexCode(toupper(name[index]));
-            handelSoundex(soundex, name, index, code, prevCode);
+
+/*  In the below function, "previousLetter" indicates the letter which is at the left side of the current letter
+	"previousCode" indicates the soundex code of the letter which is at the left side of the current letter
+	
+	"lastLetter" indicates the letter which is at the 2nd left of the current letter.
+	"lastCode" indicates the soundex code of the letter which is at the 2nd left of the current letter
+	"iterator" refers to iterator pointing to current letter	*/
+
+void handleSoundex(std::string& soundex, const std::string& name) {
+    char previousLetter = toupper(name[0]);
+    char previousCode = getSoundexCode(name[0]);
+    char lastLetter = toupper(name[0]);
+    char lastCode = getSoundexCode(name[0]);
+    for (size_t iterator = 1; iterator < name.length() && soundex.length() < 4; ++iterator) {
+      generateSoundexForLetter(name, iterator, soundex, previousCode, lastCode);
+        lastCode = previousCode;
+
+        previousCode = getSoundexCode(name[iterator]);
     }
-    soundex.resize(4, '0');
 }
+
 std::string generateSoundex(const std::string& name) {
     if (name.empty()) return "";
     std::string soundex(1, toupper(name[0]));
-    createSoundexCode(soundex, name);
+    handleSoundex(soundex, name);
+    makeSoundeLengthFour(soundex);
     return soundex;
 }
